227c0a59c0cbe33df400af4c9d8691628e0205ac
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-02-13 12:19:17 +0000

    next fix

--- vars/lvRunVi.groovy
+++ vars/lvRunVi.groovy
@@ -1,4 +1,4 @@
-def call(vi,args, lvVersion){
+def call(vi, args, lvVersion){
    echo "Running $vi."
    def logFileName = getLogName(vi)
    labviewcli("-OperationName RunVI -VIPath \"$WORKSPACE\\$vi\" $args -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
