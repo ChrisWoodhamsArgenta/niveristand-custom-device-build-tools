65c734479f6b287280c6dd1eda41ff9e38f387c7
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-02-13 12:11:03 +0000

    trying to run with args

--- vars/lvRunVi.groovy
+++ vars/lvRunVi.groovy
@@ -1,5 +1,5 @@
 def call(vi,args, lvVersion){
    echo "Running $vi."
    def logFileName = getLogName(vi)
-   labviewcli("-OperationName RunVI -VIPath \"$WORKSPACE\\$vi\" -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
+   labviewcli("-OperationName RunVI -VIPath \"$WORKSPACE\\$vi\" $args -LogFilePath \"$WORKSPACE\\lvRunVi_${logFileName}.log\"", lvVersion)
 }
