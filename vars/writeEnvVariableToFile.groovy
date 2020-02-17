3552ba867f4e8c4816709fee402cb0ccd4e4cbb9
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-02-17 15:06:10 +0000

    dd

--- vars/writeEnvVariableToFile.groovy
+++ vars/writeEnvVariableToFile.groovy
@@ -1,9 +1,9 @@
 def call(variable, path){
    echo "writing enviroment variable to a file"
    // not finished it will output only GIT_BRANCH
-	println env.JOB_NAME
-	println env.GIT_BRANCH
-	def myVar = env.GIT_BRANCH
-	println myVar
+	echo script.env.JOB_NAME
+	echo script.env.GIT_BRANCH
+	def myVar = script.env.GIT_BRANCH
+	echo myVar
      bat "echo $myVar > \"$WORKSPACE\\${path}\""
 }
