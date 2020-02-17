1c1baab182cdeb4ab68ee524ad016d7dd3ec2357
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-02-17 15:58:49 +0000

    better

--- src/ni/vsbuild/steps/WriteEnvVariableToFileStep.groovy
+++ src/ni/vsbuild/steps/WriteEnvVariableToFileStep.groovy
@@ -6,16 +6,14 @@ class WriteEnvVariableToFileStep extends LvStep {
 
    def variable
    def path
-   def variable_value
 
    WriteEnvVariableToFileStep(script, mapStep, lvVersion) {
       super(script, mapStep, lvVersion)
       this.variable = mapStep.get('variable')
 	  this.path = mapStep.get('path')
-	  this.variable_value = script.env.GIT_BRANCH
    }
 
    void executeStep(BuildConfiguration configuration) {
-      script.writeEnvVariableToFile(variable, variable_value, path)
+      script.writeEnvVariableToFile(variable, path)
    }
 }
--- vars/writeEnvVariableToFile.groovy
+++ vars/writeEnvVariableToFile.groovy
@@ -1,8 +1,6 @@
-def call(variable, variable_value, path){
+def call(variable, path){
    echo "writing enviroment variable to a file"
    // not finished it will output only BRANCH_NAME
-	def myVar = variable_value
-	echo myVar
 	myVar = env.BRANCH_NAME
 	echo myVar
      bat "echo %BRANCH_NAME%> \"$WORKSPACE\\${path}\""
