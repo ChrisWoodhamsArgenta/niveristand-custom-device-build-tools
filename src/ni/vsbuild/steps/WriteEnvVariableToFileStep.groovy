c55df944b8669c1a13167e361aaf363a0aee0132
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-02-17 15:26:34 +0000

    dd

--- src/ni/vsbuild/steps/WriteEnvVariableToFileStep.groovy
+++ src/ni/vsbuild/steps/WriteEnvVariableToFileStep.groovy
@@ -6,14 +6,16 @@ class WriteEnvVariableToFileStep extends LvStep {
 
    def variable
    def path
+   def variable_value
 
    WriteEnvVariableToFileStep(script, mapStep, lvVersion) {
       super(script, mapStep, lvVersion)
       this.variable = mapStep.get('variable')
 	  this.path = mapStep.get('path')
+	  this.variable_value = script.env.GIT_BRANCH
    }
 
    void executeStep(BuildConfiguration configuration) {
-      script.writeEnvVariableToFile(variable, path)
+      script.writeEnvVariableToFile(variable, variable_value, path)
    }
 }
