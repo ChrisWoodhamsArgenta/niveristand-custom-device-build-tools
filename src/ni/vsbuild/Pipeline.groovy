29b9e91fda2db19d68ff67b08685ed5a9d94d345
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-02-20 08:51:08 +0000

    yet another go

--- src/ni/vsbuild/Pipeline.groovy
+++ src/ni/vsbuild/Pipeline.groovy
@@ -157,7 +157,10 @@ class Pipeline implements Serializable {
 		def trigger_cause = "${script.currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause')}"
 		script.echo trigger_cause
 		
-		if(trigger=='[[_class:hudson.triggers.TimerTrigger$TimerTriggerCause, shortDescription:Started by timer]]')
+		if(trigger=='[]')
+		{
+			
+		}else
 		{
 			trigger = 'timer'
 		}
