1bfd714a83bc413af9dca1882d8056daa5742bd6
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-03-18 12:22:22 +0000

    fix

--- vars/publishNIPKG.groovy
+++ vars/publishNIPKG.groovy
@@ -4,7 +4,7 @@ def call(feed, package){
    // http://www.ni.com/documentation/en/ni-package-manager/18.5/manual/build-package-using-cli/
    //   def nipkgExePath = "$programFiles\\National Instruments\\NI Package Manager\\nipkg.exe"
    def nipkgExePath = "nipkg.exe"
-   package_path = "$WORKSPACE\\Built\\installer\\cobham-software_1.1.8_windows_x64.nipkg"
+   def package_path = "$WORKSPACE\\Built\\installer\\cobham-software_1.1.8_windows_x64.nipkg"
    bat "\"$nipkgExePath\" feed-add-pkg \"$feed\" \"$package_path\""
 
 }
