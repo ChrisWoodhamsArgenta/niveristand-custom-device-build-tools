46d040add2dba3781273a40e17765c35da3d6577
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-03-18 17:18:42 +0000

    more versitile package pub

--- vars/publishNIPKG.groovy
+++ vars/publishNIPKG.groovy
@@ -3,8 +3,9 @@ def call(feed, pkg_name){
 
    // http://www.ni.com/documentation/en/ni-package-manager/18.5/manual/build-package-using-cli/
    //   def nipkgExePath = "$programFiles\\National Instruments\\NI Package Manager\\nipkg.exe"
+   def packagePath = bat returnStdout: true, script: "GetPackageName.bat $pkg_name"
    def publishScript = "PublishPackage.bat"
-   def package_path = "$WORKSPACE\\Built\\installer\\cobham-software_1.1.9_windows_x64.nipkg"
+   def package_path = "$WORKSPACE\\Built\\installer\\$packagePath"
    bat "\"$publishScript\" \"$feed\" \"$package_path\""
 
 }
