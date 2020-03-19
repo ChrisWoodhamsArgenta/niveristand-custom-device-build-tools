ee19d5b6728b0bd4985c557a20e7afcb12ec3af2
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-03-19 10:34:09 +0000

    tmp

--- vars/publishNIPKG.groovy
+++ vars/publishNIPKG.groovy
@@ -1,11 +1,15 @@
 def call(feed, pkg_name){
    def programFiles = getWindowsVar("PROGRAMFILES")
-
+   def package_folder_path = "$WORKSPACE\\$pkg_name"
    // http://www.ni.com/documentation/en/ni-package-manager/18.5/manual/build-package-using-cli/
    //   def nipkgExePath = "$programFiles\\National Instruments\\NI Package Manager\\nipkg.exe"
-   def package_folder_path = "$WORKSPACE\\$pkg_name"
+   
+   def nipkgOutput = bat returnStdout: true, script: "echo off & dir \"$package_folder_path\\*.nipkg\" /B & echo on"
+   nipkgOutput = (nipkgOutput =~ /[\w\-\+\.]+.nipkg/)[0]
+   def packageName = nipkgOutput.trim()
+   
+
    bat "GetPackageName.bat $package_folder_path"
-   def packageName = bat "GetPackageName.bat $package_folder_path"
    //def packageName = bat returnStdout: true, script: "GetPackageName.bat $package_folder_path"
    def publishScript = "PublishPackage.bat"
    def package_path = "$WORKSPACE\\Built\\installer\\$packageName"
