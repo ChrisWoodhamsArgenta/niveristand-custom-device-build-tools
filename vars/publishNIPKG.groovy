c68ffbbf5e6bedda83daca9624416942edd476af
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-03-19 09:44:33 +0000

    tmp

--- vars/publishNIPKG.groovy
+++ vars/publishNIPKG.groovy
@@ -3,6 +3,7 @@ def call(feed, pkg_name){
 
    // http://www.ni.com/documentation/en/ni-package-manager/18.5/manual/build-package-using-cli/
    //   def nipkgExePath = "$programFiles\\National Instruments\\NI Package Manager\\nipkg.exe"
+   bat "GetPackageName.bat $pkg_name"
    def packagePath = bat returnStdout: true, script: "GetPackageName.bat $pkg_name"
    def publishScript = "PublishPackage.bat"
    def package_path = "$WORKSPACE\\Built\\installer\\$packagePath"
