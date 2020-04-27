f32b808acc2e9a87f20e958fe843efd7364fbd11
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-27 17:59:10 +0100

    fix

--- vars/createdir.groovy
+++ vars/createdir.groovy
@@ -1,8 +1,8 @@
 def call(directory){
-	def tokens = directory.tokenize("\")
+	def tokens = directory.tokenize("\\")
 	def finaldir = ""
 	tokens.each{ token ->
-		finaldir = finaldir + token + "\"
+		finaldir = finaldir + token + "\\"
 		bat "(mkdir $finaldir) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL=0"
 	}
 }
