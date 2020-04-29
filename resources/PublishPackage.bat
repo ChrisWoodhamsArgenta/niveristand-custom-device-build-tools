9172bbd1bcdaedfe18475db715c3da780b0bd46f
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-29 16:29:23 +0100

    another fix

--- resources/PublishPackage.bat
+++ resources/PublishPackage.bat
@@ -1,4 +1,4 @@
-Powershell.exe -executionpolicy remotesigned -File PublishPackage.ps1 -feed %1 -package_src_path %2
+Powershell.exe -executionpolicy remotesigned -File %~dp0\PublishPackage.ps1 -feed %1 -package_src_path %2
 IF %ERRORLEVEL% NEQ 0 ( 
 	EXIT %ERRORLEVEL%
 )
