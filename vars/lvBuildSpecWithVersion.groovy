afed68906a191dd4733bd1ea627589e9aa9885a6
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-30 17:07:47 +0100

    adding qutation marks

--- vars/lvBuildSpecWithVersion.groovy
+++ vars/lvBuildSpecWithVersion.groovy
@@ -4,7 +4,7 @@ def call(project, target, spec, lvVersion){
    vi_path = "$WORKSPACE\\niveristand-custom-device-build-tools\\lv\\utils\\modify_build_number.vi"
    version_file_path = "$WORKSPACE\\version.txt"
    build_file_path = "$WORKSPACE\\buildnumber.txt"
-   args = "$project $target $spec $version_file_path $build_file_path"
+   args = "\"$project\" \"$target\" \"$spec\" \"$version_file_path\" \"$build_file_path\""
    lvRunVi(vi_path,args,lvVersion)
    labviewcli("-OperationName ExecuteBuildSpec -ProjectPath \"$WORKSPACE\\$project\" -TargetName \"$target\" -BuildSpecName \"$spec\" -LogFilePath \"$WORKSPACE\\lvBuildSpec_${spec}.log\"", lvVersion)
 }
