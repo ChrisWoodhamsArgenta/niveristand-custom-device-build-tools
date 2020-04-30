426d34e0c6ab1d6e5b66b8de0ae3e7905d6fc3b4
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-30 17:02:26 +0100

    correcting path

--- vars/lvBuildSpecWithVersion.groovy
+++ vars/lvBuildSpecWithVersion.groovy
@@ -1,7 +1,7 @@
 def call(project, target, spec, lvVersion){
    echo "Building the build spec: $spec under target $target in project at $project"
    
-   vi_path = "$WORKSPACE\\niveristand-custom-device-build-tools\lv\utils\modify_build_number.vi"
+   vi_path = "$WORKSPACE\\niveristand-custom-device-build-tools\\lv\\utils\\modify_build_number.vi"
    version_file_path = "$WORKSPACE\\version.txt"
    build_file_path = "$WORKSPACE\\buildnumber.txt"
    args = "$project $target $spec $version_file_path $build_file_path"
