47fc747b765bdbd8aecfab21f6ae9e0741f03d1c
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-30 11:49:53 +0100

    writing build number to file

--- vars/writeEnvVariableToFile.groovy
+++ vars/writeEnvVariableToFile.groovy
@@ -1,7 +1,11 @@
 def call(variable, path){
    echo "writing enviroment variable to a file"
    // not finished it will output only BRANCH_NAME
-	myVar = env.BRANCH_NAME
+   myVar = env.BRANCH_NAME
+   if(variable == BUILD_NUMBER)
+   {
+	myVar =env.BUILD_NUMBER
+   }
 	echo myVar
      bat "echo %BRANCH_NAME%> \"$WORKSPACE\\${path}\""
 }
