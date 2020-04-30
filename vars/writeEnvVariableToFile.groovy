d3dda9ccacb58fd95775b39defb1544880cfcfe3
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-30 13:43:55 +0100

    another fix

--- vars/writeEnvVariableToFile.groovy
+++ vars/writeEnvVariableToFile.groovy
@@ -1,7 +1,7 @@
 def call(variable, path){
    echo "writing enviroment variable to a file"
    // not finished it will output only BRANCH_NAME
-   if(variable == BUILD_NUMBER)
+   if(variable == 'BUILD_NUMBER')
    {
 	bat "echo %BUILD_NUMBER%> \"$WORKSPACE\\${path}\""
    }
