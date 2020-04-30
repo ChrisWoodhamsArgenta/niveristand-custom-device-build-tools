b36d301461ca301dd54aae7780181e9beb8747f8
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-04-30 12:36:14 +0100

    fix

--- resources/publish_gpm.bat
+++
@@ -1,2 +0,0 @@
-%~dp0\updated_gpm_build_number %1
-gpm publish
--- vars/publishGPM.groovy
+++ vars/publishGPM.groovy
@@ -1,3 +1,5 @@
 def call(feed, pkg_name){
-   bat "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\publish_gpm.bat $WORKSPACE"
+
+   bat "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\updated_gpm_build_number.bat $WORKSPACE"
+   bat "gpm pubish"
 }
