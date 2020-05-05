4d3c9e4efa738dc85bbffda550358219eea9ba5d
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-05-05 18:44:56 +0100

    fix

--- vars/lvBuildAllWithVersion.groovy
+++ vars/lvBuildAllWithVersion.groovy
@@ -1,5 +1,5 @@
 def call(project, lvVersion){
    echo "Building all build specs in project at $project"
    def logFileName = getLogName(project)
-   labviewcli("-OperationName ExecuteAllBuildSpecsWithVersion -ProjectPath \"$WORKSPACE\\$project\" -Version "1.2.3.4" -LogFilePath \"$WORKSPACE\\lvBuildAll_${logFileName}.log\"", lvVersion)
+   labviewcli("-OperationName ExecuteAllBuildSpecsWithVersion -ProjectPath \"$WORKSPACE\\$project\" -Version \"1.2.3.4\" -LogFilePath \"$WORKSPACE\\lvBuildAll_${logFileName}.log\"", lvVersion)
 }
