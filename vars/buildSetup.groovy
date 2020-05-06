6eba3ba70c1266b46716fefa22302723ab7fc94e
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-05-06 16:18:13 +0100

    fix

--- vars/buildSetup.groovy
+++ vars/buildSetup.groovy
@@ -5,6 +5,8 @@ def call(lvVersion,workspace, branchName,triggerType,nodeLabel) {
 		programFiles = getWindowsVar("PROGRAMFILES")
    }
    def baseVersion = (lvVersion =~ /^[0-9]+/).getAt(0)
+   
+   echo "LV path: $programFiles\\National Instruments\\LabVIEW ${baseVersion}\\LabVIEW.exe"
    env."labviewPath_${lvVersion}" = "$programFiles\\National Instruments\\LabVIEW ${baseVersion}\\LabVIEW.exe"
    bat "niveristand-custom-device-build-tools\\resources\\buildSetup.bat ${workspace} ${branchName} ${triggerType}"
 }
