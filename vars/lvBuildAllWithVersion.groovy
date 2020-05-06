b31023f9550350f4cd8ccc2eb3be3da69711db00
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-05-06 07:45:10 +0100

    tmp

--- vars/lvBuildAllWithVersion.groovy
+++ vars/lvBuildAllWithVersion.groovy
@@ -2,18 +2,18 @@ def call(project, lvVersion){
    echo "Building all build specs in project at $project"
    
     def baseVersion = env.BRANCH_NAME.split("[-/]")[1]
-    def versionPartCount = baseVersion.tokenize(".").size()
+    //def versionPartCount = baseVersion.tokenize(".").size()
 
-    def versionPartsToDisplay = 3
-    for(versionPartCount; versionPartCount < versionPartsToDisplay; versionPartCount++) {
-        baseVersion = "${baseVersion}.0"
-    }
+    //def versionPartsToDisplay = 3
+    //for(versionPartCount; versionPartCount < versionPartsToDisplay; versionPartCount++) {
+    //    baseVersion = "${baseVersion}.0"
+    //}
    
-	def build_num = env.BUILD_NUMBER
+	//def build_num = env.BUILD_NUMBER
 	
-	def version = $baseVersion + "." + build_num
-    echo "$version"
+	//def version = $baseVersion + "." + build_num
+    echo "$baseVersion"
    
    def logFileName = getLogName(project)
-   labviewcli("-OperationName ExecuteAllBuildSpecsWithVersion -ProjectPath \"$WORKSPACE\\$project\" -Version \"$version\" -LogFilePath \"$WORKSPACE\\lvBuildAll_${logFileName}.log\"", lvVersion)
+   labviewcli("-OperationName ExecuteAllBuildSpecsWithVersion -ProjectPath \"$WORKSPACE\\$project\" -Version \"1.2.3.4\" -LogFilePath \"$WORKSPACE\\lvBuildAll_${logFileName}.log\"", lvVersion)
 }
