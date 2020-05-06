befe289fcdf580dc493d1d13d70d9d911d97997f
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-05-06 14:44:54 +0100

    adding x64 version resolution

--- vars/buildSetup.groovy
+++ vars/buildSetup.groovy
@@ -1,5 +1,8 @@
 def call(lvVersion,workspace, branchName,triggerType,nodeLabel) {
    def programFiles = getWindowsVar("PROGRAMFILES(x86)")
+   if(nodeLabel.grep("x64")){
+		programFiles = getWindowsVar("PROGRAMFILES")
+   }
    def baseVersion = (lvVersion =~ /^[0-9]+/).getAt(0)
    env."labviewPath_${lvVersion}" = "$programFiles\\National Instruments\\LabVIEW ${baseVersion}\\LabVIEW.exe"
    bat "niveristand-custom-device-build-tools\\resources\\buildSetup.bat ${workspace} ${branchName} ${triggerType}"
