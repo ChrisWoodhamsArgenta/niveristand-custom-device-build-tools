2aab4ca2374f7a69265064fcfbdd6623b46eaab6
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-05-14 11:44:28 +0100

    simple way

--- resources/install_and_test.ps1
+++ resources/install_and_test.ps1
@@ -4,9 +4,41 @@ nipkg install $package_name -y --accept-eulas
 
 $PSScriptRoot
 
-$ScriptToRun = $PSScriptRoot+"\test_app.ps1 -exe_path " + $exe_path +" -version "+ $version
+#$ScriptToRun = $PSScriptRoot+"\test_app.ps1 -exe_path " + $exe_path +" -version "+ $version
 
-&$ScriptToRun
+#$ScriptToRun
+
+
+$string = '&"{0}" version' -f $exe_path
+$string
+
+
+$scriptBlock = [Scriptblock]::Create($string)
+#$code = { "C:\Argenta\Tenneco_EGDRigs500V\CAN File Converter\CAN File Converter.bat version"}
+$code = $scriptBlock
+
+$timeoutSeconds = 20 # set your timeout value here
+$j = Start-Job -ScriptBlock $code
+Wait-Job $j -Timeout $timeoutSeconds | out-null
+if ($j.State -eq "Completed") 
+{
+    $result = Receive-Job -Job $j
+    $result
+    #if($result -eq "1.1.1.1"){ "Success" }
+    #else { throw "version not equalto 1.1.1.2" }
+    $msg = "Success! Version: " + $result + " installed."+"Shoudl be:"+$version
+    $msg
+    
+
+}
+elseif ($j.State -eq "Running") { "interrupted" }
+else 
+{ 
+   throw "up not started within 10 sec"
+}
+
+Remove-Job -force $j #cleanup
+
 
 
 
