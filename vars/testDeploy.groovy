4ced72c2df7a76008b32f0afed5d11c535328b98
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-05-14 12:18:23 +0100

    another go

--- vars/testDeploy.groovy
+++ vars/testDeploy.groovy
@@ -24,7 +24,7 @@ def call(feed,pkg_name,exe_path,lvVersion){
    echo "$version"
    
    // now build and test packages
-   def instalANDtestScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\install_and_test.bat"
+   //def instalANDtestScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\install_and_test.bat"
    def installScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\install_package.bat"
    def testScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\test_app.bat"
    bat "nipkg add-feed --name=test_feed $feed"
