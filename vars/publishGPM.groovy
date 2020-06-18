9ba3f2ab395ebdcc432b3fdcba2ae7c42177e2d9
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-06-18 13:45:06 +0100

    tmp

--- vars/publishGPM.groovy
+++ vars/publishGPM.groovy
@@ -2,7 +2,9 @@ def call(feed, pkg_name){
    //bat "mkdir $WORKSPACE\\gpm_tmp"
    //bat "mkdir $WORKSPACE\\gpm_tmp\\source"
    bat "xcopy /E /I $WORKSPACE\\source $WORKSPACE\\gpm_tmp\\source"
-   bat "xcopy /f /y $WORKSPACE\\gpackage.json $WORKSPACE\\gpm_tmp\\gpackage.json"
+   
+   //https://stackoverflow.com/questions/3018289/xcopy-file-rename-suppress-does-xxx-specify-a-file-name-message
+   bat "echo f | xcopy /f /y $WORKSPACE\\gpackage.json $WORKSPACE\\gpm_tmp\\gpackage.json"
   
    bat "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\updated_gpm_build_number.bat $WORKSPACE"
    bat "cd $WORKSPACE\\gpm_tmp & gpm publish"
