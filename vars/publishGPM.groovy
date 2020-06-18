a7430a9950bbfe3185cda52738b635d406b60adf
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-06-18 13:21:40 +0100

    go

--- vars/publishGPM.groovy
+++ vars/publishGPM.groovy
@@ -1,8 +1,8 @@
 def call(feed, pkg_name){
-   bat "mkdir $WORKSPACE\\gpm_tmp"
-   bat "mkdir $WORKSPACE\\gpm_tmp\\source"
-   bat "robocopy $WORKSPACE\\source $WORKSPACE\\gpm_tmp\\source /nfl /ndl /njh /njs /nc /ns /np /COPY:DT /mir"
-  
+   //bat "mkdir $WORKSPACE\\gpm_tmp"
+   //bat "mkdir $WORKSPACE\\gpm_tmp\\source"
+   bat "xcopy $WORKSPACE\\source $WORKSPACE\\gpm_tmp\\source"
+ 
    bat "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\updated_gpm_build_number.bat $WORKSPACE"
    bat "gpm publish"
 }
