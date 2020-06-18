c4b48199e3c74f6a614e3deb5da6d8f6c431ac64
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-06-18 13:51:24 +0100

    fix for build number

--- vars/publishGPM.groovy
+++ vars/publishGPM.groovy
@@ -3,9 +3,11 @@ def call(feed, pkg_name){
    //bat "mkdir $WORKSPACE\\gpm_tmp\\source"
    bat "xcopy /E /I $WORKSPACE\\source $WORKSPACE\\gpm_tmp\\source"
    
+   bat "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\updated_gpm_build_number.bat $WORKSPACE"
+   
    //https://stackoverflow.com/questions/3018289/xcopy-file-rename-suppress-does-xxx-specify-a-file-name-message
    bat "echo f | xcopy /f /y $WORKSPACE\\gpackage.json $WORKSPACE\\gpm_tmp\\gpackage.json"
   
-   bat "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\updated_gpm_build_number.bat $WORKSPACE"
+
    bat "cd $WORKSPACE\\gpm_tmp & gpm publish"
 }
