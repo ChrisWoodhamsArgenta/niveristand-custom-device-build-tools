90148d5ed318f6ac24a883d4d0565147833c82b9
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-10-22 11:50:04 +0100

    clean up

--- vars/nipkgBuildAndPublish.groovy
+++ vars/nipkgBuildAndPublish.groovy
@@ -1,16 +1,9 @@
 def call(feed, pkg_name,pkg_version,dependencies,package_dst_path,package_src_path){
-   def programFiles = getWindowsVar("PROGRAMFILES")
-   def package_folder_path = "$WORKSPACE\\$pkg_name"
 
-	//def pkg_version = "0.0.0-1"
-	//def dependencies = ""
-	//def package_dst_path = "BootVolume\\Argenta\\TestApp"
-	def package_abs_src_path = "$WORKSPACE\\$Built"
-	
+	def package_abs_src_path = "$WORKSPACE\\$package_src_path"
 	def pkg_dir = "$WORKSPACE\\$nipkg"
   
-   def buildandpublishScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\NIPKGBuildAndPublish.bat"
+	def buildandpublishScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\NIPKGBuildAndPublish.bat"
    
-   bat "\"$buildandpublishScript\" \"$feed\" \"$pkg_name\" \"$pkg_version\" \"$dependencies\" \"$package_dst_path\"  \"$package_abs_src_path\" \"$pkg_dir\" " 
-   
+	bat "\"$buildandpublishScript\" \"$feed\" \"$pkg_name\" \"$pkg_version\" \"$dependencies\" \"$package_dst_path\"  \"$package_abs_src_path\" \"$pkg_dir\" "   
 }
