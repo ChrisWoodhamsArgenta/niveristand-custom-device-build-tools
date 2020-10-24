4c51c5d059ec24fdb741aa785fccba890a335616
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-10-24 11:12:30 +0100

    debug info

--- vars/nipkgBuildAndPublish.groovy
+++ vars/nipkgBuildAndPublish.groovy
@@ -1,5 +1,5 @@
 def call(feed, pkg_name,pkg_version,dependencies,package_dst_path,package_src_path){
-
+	echo "Building and publishing package."
 	def package_abs_src_path = "$WORKSPACE\\$package_src_path"
 	def pkg_dir = "$WORKSPACE\\$nipkg"
   
