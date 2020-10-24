025ce82437bd3c7c153751a339a564f06cb729e0
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-10-24 11:53:20 +0100

    add try catch

--- resources/PublishPackage.ps1
+++ resources/PublishPackage.ps1
@@ -1,7 +1,17 @@
 param([string] $feed, [string] $package_src_path)
 
-$files = Get-ChildItem -Path $package_src_path
-$package_name = $files.Name
-$package_dst_path = $feed+"\" + $package_name
-Copy-Item -Path $package_src_path -Destination $feed 
-nipkg feed-add-pkg $feed $package_dst_path
+$ErrorActionPreference  = "Stop"
+
+Try
+{
+    $files = Get-ChildItem -Path $package_src_path
+    $package_name = $files.Name
+    $package_dst_path = $feed+"\" + $package_name
+    Copy-Item -Path $package_src_path -Destination $feed 
+    nipkg feed-add-pkg $feed $package_dst_path
+    exit 0
+}
+catch
+{
+throw
+}
