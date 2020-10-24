38c649c106a65a747248bfb1765d7e9011924e4b
DawidWoznyArgenta <dawid.wozny@argentaconsult.com>
2020-10-24 11:33:20 +0100

    fix

--- vars/nipkgBuildAndPublish.groovy
+++ vars/nipkgBuildAndPublish.groovy
@@ -1,7 +1,7 @@
 def call(feed, pkg_name,pkg_version,dependencies,package_dst_path,package_src_path){
 	echo "Building and publishing package."
 	def package_abs_src_path = "$WORKSPACE\\$package_src_path"
-	def pkg_dir = "$WORKSPACE\\$nipkg"
+	def pkg_dir = "$WORKSPACE\\nipkg"
   
 	def buildandpublishScript = "$WORKSPACE\\niveristand-custom-device-build-tools\\resources\\NIPKGBuildAndPublish.bat"
    
